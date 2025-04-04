2f33645ed9ac77444fa25b6a0a2b91992fb6f169
João Paulo <joaowais@gmail.com>
2025-04-04 17:26:11 -0300

    first commit

---
+++ src/main/java/com/jhops10/controle_estoque/ControleEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.controle_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEstoqueApplication.class, args);
+	}
+
+}
