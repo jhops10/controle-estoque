5decafbe80842f5cae6a435fba53cbeae94298f4
João Paulo <joaowais@gmail.com>
2025-04-08 22:48:12 -0300

    adiciona método findProductWithLowStock para ProductRepository

--- src/main/java/com/jhops10/controle_estoque/domain/repository/ProductRepository.java
+++ src/main/java/com/jhops10/controle_estoque/domain/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.jhops10.controle_estoque.domain.repository;
 
 import com.jhops10.controle_estoque.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByQuantityLessThan(Integer quantity);
+
+    @Query("SELECT p FROM Product p WHERE p.minimumStock IS NOT NULL AND p.quantity < p.minimumStock")
+    List<Product> findProductsWithLowStock();
 }
